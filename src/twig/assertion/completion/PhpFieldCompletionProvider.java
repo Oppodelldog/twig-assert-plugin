583b9acccecbd5e6fdb2d7fcf9e9f24785c73f14
Oppodelldog <odog@netcologne.de>
2019-03-13 23:22:34 +0100

    add completion for fields

---
+++ src/twig/assertion/completion/PhpFieldCompletionProvider.java
@@ -0,0 +1,29 @@
+package twig.assertion.completion;
+
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionProvider;
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.PlainPrefixMatcher;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.ProcessingContext;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.completion.PhpCompletionUtil;
+import com.jetbrains.php.completion.PhpVariantsUtil;
+import com.jetbrains.twig.TwigTokenTypes;
+import org.jetbrains.annotations.NotNull;
+import twig.assertion.util.ElementNavigator;
+import twig.assertion.util.FindElements;
+
+class PhpFieldCompletionProvider extends CompletionProvider<CompletionParameters> {
+    @Override
+    protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet) {
+        PsiElement currElement = parameters.getPosition().getOriginalElement();
+        if (currElement.getNode().getElementType() == TwigTokenTypes.IDENTIFIER && currElement.getPrevSibling().getText().equals(".")) {
+            String variableName = new ElementNavigator(currElement).prev(2).getText();
+            String phpClassname = FindElements.findAssertType(parameters.getOriginalFile(), variableName);
+            PhpIndex phpIndex = PhpIndex.getInstance(currElement.getProject());
+            PlainPrefixMatcher pm = new PlainPrefixMatcher(phpClassname.replace("\\\\", "\\"));
+            PhpCompletionUtil.getAllClasses(pm, phpIndex).forEach(phpClass -> completionResultSet.addAllElements(PhpVariantsUtil.getLookupItems(phpClass.getFields(), false, null)));
+        }
+    }
+}
--- src/twig/assertion/completion/TwigCompletionContributor.java
+++ src/twig/assertion/completion/TwigCompletionContributor.java
@@ -10,6 +10,7 @@ class TwigCompletionContributor extends CompletionContributor {
     TwigCompletionContributor() {
         this.extend(CompletionType.BASIC, PlatformPatterns.psiElement().withParent(TwigCompositeElement.class), new PhpFQCNCompletionProvider());
         this.extend(CompletionType.BASIC, PlatformPatterns.psiElement().withParent(TwigCompositeElement.class), new PhpMethodCompletionProvider());
+        this.extend(CompletionType.BASIC, PlatformPatterns.psiElement().withParent(TwigCompositeElement.class), new PhpFieldCompletionProvider());
         this.extend(CompletionType.BASIC, PlatformPatterns.psiElement().withParent(TwigCompositeElement.class), new TwigAssertCompletionProvider());
     }
 }
