e8ba29c4b8d934b0ebe477cb91be0fd71ac49db5
Oppodelldog <odog@netcologne.de>
2019-03-18 23:23:37 +0100

    a bit cleaner

--- src/main/java/twig/assertion/refactoring/RefactoringProvider.java
+++ src/main/java/twig/assertion/refactoring/RefactoringProvider.java
@@ -39,24 +39,30 @@ class RefactoringProvider extends RefactoringScopeElementListenerProvider {
             }
         }
 
+        private static void replaceTwigString(Project project, PsiElement assertElement, String replaceText) {
+            String template = "{{ set x = \"" + replaceText + "\" }}";
+            PsiElement newItem = TwigElementFactory.createPsiElement(project, template, TwigTokenTypes.STRING_TEXT);
+            if (newItem != null) {
+                assertElement.replace(newItem);
+            }
+        }
+
         @Override
         protected void elementRenamedOrMoved(@NotNull PsiElement psiElement) {
             if (psiElement instanceof PhpClass) {
-                String previousFQCN = TwigFqn.toTwigString(this.originalFqn);
-                String replaceWith = TwigFqn.toTwigString(((PhpClass) psiElement).getFQN());
                 final Project project = psiElement.getProject();
+                String previousFQCN = TwigFqn.toTwigString(this.originalFqn);
+                String replaceText = TwigFqn.toTwigString(((PhpClass) psiElement).getFQN());
                 GlobalSearchScope searchScope = GlobalSearchScope.allScope(project);
-
-                PsiSearchHelper.getInstance(project).processAllFilesWithWordInLiterals(ASSERT_TAG_NAME, searchScope, psiFile -> {
+                PsiSearchHelper psiSearchHelper = PsiSearchHelper.getInstance(project);
+                psiSearchHelper.processAllFilesWithWordInLiterals(ASSERT_TAG_NAME, searchScope, psiFile -> {
                     if (psiFile.getLanguage() == TwigLanguage.INSTANCE) {
-                        FindElements.findAssertPsiElementsByFQCN(psiFile, previousFQCN).forEach(assertElement -> {
-                            CommandProcessor.getInstance().executeCommand(project, () -> ApplicationManager.getApplication().runWriteAction(() -> {
-                                PsiElement newItem = TwigElementFactory.createPsiElement(project, "{{ set x = \"" + replaceWith + "\" }}", TwigTokenTypes.STRING_TEXT);
-                                if (newItem != null) {
-                                    assertElement.replace(newItem);
-                                }
-                            }), "Refactor PHP Class", null);
-                        });
+                        FindElements.findAssertPsiElementsByFQCN(psiFile, previousFQCN).
+                                forEach(assertElement
+                                        -> CommandProcessor.getInstance().executeCommand(project, ()
+                                        -> ApplicationManager.getApplication().runWriteAction(()
+                                        -> replaceTwigString(project, assertElement, replaceText)
+                                ), "Refactor PHP Class", null));
                     }
                     return true;
                 });
